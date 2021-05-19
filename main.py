7e724202a1282b4fc866fbd4d2c2e3f2590baf7f
Jordan Onwodi <>
2021-05-19 15:32:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+# dunder or magic methods
+
+# x = [1,2,3,4]
+
+# print(dir(x))
+# print(dir(x.__dir__()))
+# print(x.__len__())
+# print(len(x))
+
+# print(2+2)
+# print(int.__add__(2,2))
+
+# print("c" + "r")
+# print(str.__add__("c","r"))
+# print(float.__add__(2.5,2.6))
+
+# from todo import Todo
+
+# J_todo = Todo(name = "Jordan")
+# s_todo = Todo(name = "Lianne")
+
+# print(dir(J_todo))
+
+# print(J_todo)
+# print(repr(J_todo))
+# print(repr(s_todo))
+
+# print(str(J_todo))
+# print(str(s_todo))
+# print(J_todo)
+# print(s_todo)
+
+# J_todo.add("Money")
+# J_todo.add("Cryptocurrency")
+
+# print(len(J_todo))
+# print(len(s_todo))
+
+# print(J_todo > s_todo)
+# print(J_todo < s_todo)
